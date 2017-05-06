df274f398533fd4f86a9595754c1482a1fd1b941
patrick <873948000@qq.com>
2017-05-06 20:18:34 +0800

    # Commit format:
    Branch:
        dev
    
    Main feature:
        - home page
        - user page
    
    Detailed feature:
    
        - user page (Finished)
        - user page HTML (Todo, rebuild with materialize)
        - complete profile (F)
        - edit profile (F)
        - admin edit profile (F)
        - user login(F)
        - user logout(F)
        - register(F)
        - change password(Todo)

--- MyBlog/app/auth/forms.py
+++ MyBlog/app/auth/forms.py
@@ -7,20 +7,20 @@ from ..models import Account, UserInfo
 
 class LoginForm(FlaskForm):
 
-    email = StringField('Email:', validators=[DataRequired(), Email(), Length(1, 64)])
-    password = PasswordField('Password:', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    email = StringField('邮箱:', validators=[DataRequired(), Email(), Length(1, 64)])
+    password = PasswordField('密码:', validators=[DataRequired()])
+    remember_me = BooleanField('记住密码')
+    submit = SubmitField('确认登录')
 
 
 class RegisterForm(FlaskForm):
 
-    email = StringField('Email:', validators=[DataRequired(), Email(), Length(1, 64)])
-    username = StringField('User Name:', validators=[DataRequired(), Length(1, 64)])
-    phone = StringField('Phone Number:', validators=[DataRequired(), Length(11, 11)])
-    password = PasswordField('Password:', validators=[DataRequired(), EqualTo('password2', message='两次密码应该相同。')])
-    password2 = PasswordField('Confirm Password:', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    email = StringField('邮箱：', validators=[DataRequired(), Email(), Length(1, 64)])
+    username = StringField('用户名：', validators=[DataRequired(), Length(1, 64)])
+    phone = StringField('电话号码：', validators=[DataRequired(), Length(11, 11)])
+    password = PasswordField('密码：', validators=[DataRequired(), EqualTo('password2', message='两次密码应该相同。')])
+    password2 = PasswordField('重复密码：', validators=[DataRequired()])
+    submit = SubmitField('确认注册')
 
     def validate_email(self, field):
         if Account.query.filter_by(email=field.data).first():
--- MyBlog/app/auth/views.py
+++ MyBlog/app/auth/views.py
@@ -12,13 +12,10 @@ from flask_login import login_user, logout_user, login_required, current_user
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        info = UserInfo.query.filter_by(email=form.email.data).first()
-        if info is not None:
-            uid = info.uid
-            user = Account.query.get(uid)
-            if user is not None and user.verify_password(form.password.data):
-                login_user(user, form.remember_me.data)
-                return redirect(request.args.get('next') or url_for('main.index'))
+        user = Account.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
         flash('用户名或密码错误。')
     return render_template('auth/login.html', form=form)  # 路径是相对templates的
 
@@ -63,9 +60,9 @@ def confirm(token):
     if current_user.confirmed:
         return redirect(url_for('main.index'))
     if current_user.confirm(token):
-        flash('You have confirmed your account. Thanks!')
+        flash('你的账户已经成功确认！')
     else:
-        flash('The confirmation link is invalid or has expired.')
+        flash('确认链接已经失效！')
     return redirect(url_for('main.index'))
 
 
@@ -92,94 +89,92 @@ def unconfirmed():
 @login_required
 def resend_confirmation():
     token = current_user.generate_confirmation_token()
-    userinfo = UserInfo.query.get(current_user.uid)
-    send_email(userinfo.email, 'Confirm Your Account',
-               'auth/email/confirm', user=current_user, userinfo=userinfo, token=token)
-    flash('A confirmation email has been sent to your email.')
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('确认邮件已经发送到了你的邮箱，请注意查收！')
     return redirect(url_for('main.index'))
 
 
-@auth.route('/changepasswd', methods=['GET', 'POST'])
-@login_required
-def change_password():
-    form = ChangePassForm()
-    if form.validate_on_submit():
-        if current_user.verify_password(form.former_pass.data):
-            current_user.password = form.password.data
-            db.session.add(current_user)
-            db.session.commit()
-            flash('成功更改密码！')
-            return redirect(url_for('main.index'))
-        flash('请输入正确的密码！')
-    return render_template('auth/password.html', form=form)
-
-
-@auth.route('/forgetpasswd', methods=['GET', 'POST'])
-def forget_password():  # 发送验证邮件
-    form = VerifyEmailForm()
-    if form.validate_on_submit():
-        verify = Verify(form.email.data)
-        token = verify.generate_confirmation_token()
-        send_email(form.email.data, 'Verify Your Email',
-                   'auth/email/verify', token=token, email=form.email.data)
-        flash('A verification email has been sent to your email. If you didn\'t receive the email,'
-              ' go back to the previous page and check your email.')
-        return redirect(url_for('main.index'))
-    return render_template('auth/submit_email.html', form=form)
-
-
-@auth.route('/resetpasswd/<token>/<email>', methods=['GET', 'POST'])
-def reset_password(token, email):
-    verify = Verify(email)
-    userinfo = UserInfo.query.filter_by(email=email).first()
-    if not userinfo:
-        flash('The email is invalid!')
-        return redirect(url_for('auth.forget_password'))
-    if verify.confirm(token):
-        flash('You have verified your account. And you can reset your password now!')
-        form = ResetPassForm()
-        if form.validate_on_submit():
-            user = Account.query.get(userinfo.uid)
-            user.password = form.password.data  # 修改密码要这样，其中user.password并不是数据库对应的密码字段
-            login_user(user, False)
-            db.session.add(user)
-            db.session.commit()
-            flash('成功重置密码！')
-            return redirect(url_for('main.index'))
-        return render_template('auth/reset_pass.html', form=form)
-    else:
-        flash('The confirmation link is invalid or has expired.')
-        return redirect(url_for('main.index'))
-
-
-@auth.route('/changemail', methods=['GET', 'POST'])
-@login_required
-def check_mail():  # 发送验证邮件
-    form = VerifyEmailForm()
-    if form.validate_on_submit():
-        token = current_user.generate_confirmation_token()
-        email = form.email.data
-        send_email(email, 'Verify Your Email',
-                   'auth/email/verify_mail', token=token, email=email)
-        flash('A verification email has been sent to your email. If you didn\'t receive the email,'
-              ' go back to the previous page and check your email.')
-        return redirect(url_for('main.index'))
-    return render_template('auth/reset_mail.html', form=form)
-
-
-@auth.route('/changemail/<token>/<email>', methods=['GET', 'POST'])
-@login_required
-def change_mail(token, email):
-    if current_user.confirm(token):
-        flash('You have verified your account. And you can change your email now!')
-        userinfo = UserInfo.query.filter_by(uid=current_user.uid).first()
-        userinfo.email = email
-        userinfo.avatar_hash = hashlib.md5(email.encode('utf-8')).hexdigest()
-        db.session.add(userinfo)
-        db.session.commit()
-        flash('成功修改邮箱！')
-        return redirect(url_for('main.index'))
-    else:
-        flash('The confirmation link is invalid or has expired.')
-        return redirect(url_for('main.index'))
+# @auth.route('/changepasswd', methods=['GET', 'POST'])
+# @login_required
+# def change_password():
+#     form = ChangePassForm()
+#     if form.validate_on_submit():
+#         if current_user.verify_password(form.former_pass.data):
+#             current_user.password = form.password.data
+#             db.session.add(current_user)
+#             db.session.commit()
+#             flash('成功更改密码！')
+#             return redirect(url_for('main.index'))
+#         flash('请输入正确的密码！')
+#     return render_template('auth/password.html', form=form)
+#
+#
+# @auth.route('/forgetpasswd', methods=['GET', 'POST'])
+# def forget_password():  # 发送验证邮件
+#     form = VerifyEmailForm()
+#     if form.validate_on_submit():
+#         verify = Verify(form.email.data)
+#         token = verify.generate_confirmation_token()
+#         send_email(form.email.data, 'Verify Your Email',
+#                    'auth/email/verify', token=token, email=form.email.data)
+#         flash('一封确认邮件已经发送到了你的邮箱。如果你没有收到有钱，氢回到上一页重新发送。')
+#         return redirect(url_for('main.index'))
+#     return render_template('auth/submit_email.html', form=form)
+#
+#
+# @auth.route('/resetpasswd/<token>/<email>', methods=['GET', 'POST'])
+# def reset_password(token, email):
+#     verify = Verify(email)
+#     userinfo = UserInfo.query.filter_by(email=email).first()
+#     if not userinfo:
+#         flash('The email is invalid!')
+#         return redirect(url_for('auth.forget_password'))
+#     if verify.confirm(token):
+#         flash('You have verified your account. And you can reset your password now!')
+#         form = ResetPassForm()
+#         if form.validate_on_submit():
+#             user = Account.query.get(userinfo.uid)
+#             user.password = form.password.data  # 修改密码要这样，其中user.password并不是数据库对应的密码字段
+#             login_user(user, False)
+#             db.session.add(user)
+#             db.session.commit()
+#             flash('成功重置密码！')
+#             return redirect(url_for('main.index'))
+#         return render_template('auth/reset_pass.html', form=form)
+#     else:
+#         flash('The confirmation link is invalid or has expired.')
+#         return redirect(url_for('main.index'))
+#
+#
+# @auth.route('/changemail', methods=['GET', 'POST'])
+# @login_required
+# def check_mail():  # 发送验证邮件
+#     form = VerifyEmailForm()
+#     if form.validate_on_submit():
+#         token = current_user.generate_confirmation_token()
+#         email = form.email.data
+#         send_email(email, 'Verify Your Email',
+#                    'auth/email/verify_mail', token=token, email=email)
+#         flash('A verification email has been sent to your email. If you didn\'t receive the email,'
+#               ' go back to the previous page and check your email.')
+#         return redirect(url_for('main.index'))
+#     return render_template('auth/reset_mail.html', form=form)
+#
+#
+# @auth.route('/changemail/<token>/<email>', methods=['GET', 'POST'])
+# @login_required
+# def change_mail(token, email):
+#     if current_user.confirm(token):
+#         flash('You have verified your account. And you can change your email now!')
+#         userinfo = UserInfo.query.filter_by(uid=current_user.uid).first()
+#         userinfo.email = email
+#         userinfo.avatar_hash = hashlib.md5(email.encode('utf-8')).hexdigest()
+#         db.session.add(userinfo)
+#         db.session.commit()
+#         flash('成功修改邮箱！')
+#         return redirect(url_for('main.index'))
+#     else:
+#         flash('The confirmation link is invalid or has expired.')
+#         return redirect(url_for('main.index'))
 
